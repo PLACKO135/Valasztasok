22a5c6de7e7348e88f54ab154a6d2586b5f1dddf
Placko135 <papplaszlolevente@gmail.com>
2024-09-26 09:16:59 +0200

    :|

--- Valasztasok/Pages/Adatok feltoltese szoveg fajlbol.cshtml.cs
+++ Valasztasok/Pages/Adatok feltoltese szoveg fajlbol.cshtml.cs
@@ -7,10 +7,10 @@ namespace Valasztasok.Pages
     public class Adatok_feltoltese_szoveg_fajlbolModel : PageModel
     {
         public IWebHostEnvironment Environment { get; set; }
-        public Valasztas_db_Context Context { get; set; }
+        public Valasztas_db_Context _context { get; set; }
         public Adatok_feltoltese_szoveg_fajlbolModel(IWebHostEnvironment webHostEnvironment, Valasztas_db_Context valasztas_Db_Context)
         {
-            Context = valasztas_Db_Context;
+            _context = valasztas_Db_Context;
             Environment = webHostEnvironment;
         }
         [BindProperty]
@@ -33,25 +33,28 @@ namespace Valasztasok.Pages
                 var line = sr.ReadLine();
                 var elemek = line.Split(" ");
                 Jelolt ujJelolt = new Jelolt();
-                Part ujPart = new Part();
-                if (!Context.Partok.Select(x => x.Rovid_nev).Contains(elemek[4]))
+                Part ujPart ;
+                if (!_context.Partok.Any(x => x.Rovid_nev==elemek[4]))
                 {
                     ujPart = new();
                     ujPart.Rovid_nev = elemek[4];
-                    Context.Partok.Add(ujPart);
+                    _context.Partok.Add(ujPart);
+                    await _context.SaveChangesAsync();
                 }
                 else
                 {
-                    ujPart= Context.Partok.Where(x => x.Rovid_nev == elemek[4]).First();
+                    ujPart= _context.Partok.Where(x => x.Rovid_nev == elemek[4]).First();
                 }
                 ujJelolt.Kerulet = int.Parse(elemek[0]);
                 ujJelolt.Szavazatszam = int.Parse(elemek[1]);
                 ujJelolt.Nev = elemek[2] + " " + elemek[3];
                 ujPart.Rovid_nev = elemek[4];
-                Context.Jeloltek.Add(ujJelolt);
-                Context.Partok.Add(ujPart);
+                _context.Jeloltek.Add(ujJelolt);
+            
             }
             sr.Close();
+            await _context.SaveChangesAsync();
+
             return Page();
         }
     }
